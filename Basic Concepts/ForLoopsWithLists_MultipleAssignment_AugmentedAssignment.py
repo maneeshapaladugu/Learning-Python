8c8fce927821f255077c3e6d6388c04303fba92f
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2021-01-26 19:57:59 +0530

    Add files via upload

--- Basic Concepts/ForLoopsWithLists_MultipleAssignment_AugmentedAssignment.py
+++ Basic Concepts/ForLoopsWithLists_MultipleAssignment_AugmentedAssignment.py
@@ -5,7 +5,7 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> for i in range(4):
 	print(i)
 
-	
+#Output:	
 0
 1
 2
@@ -13,39 +13,40 @@ Type "help", "copyright", "credits" or "license()" for more information.
 
 >>> range(4)
 range(0, 4)
+
 >>> [0,1,2,3] #range object is similar to the list
 [0, 1, 2, 3]
 >>> 
->>> for i in [0,1,2,3]:
+>>> for i in [0,1,2,3]: # list can also be used in place of range object
 	print(i)
 
-	
+#Output:
 0
 1
 2
 3
+
 >>> range(4)
 range(0, 4)
 >>> list(range(4))
 [0, 1, 2, 3]
->>> list(range(0,100,2)) #step twice from 0 to 100
+
+>>> list(range(0,100,2)) #step twice from 0 to 99
 [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98]
->>> 
->>> spam = list(range(0,100,3))#step thrice and assign to spam
->>> spam'
-SyntaxError: EOL while scanning string literal
+>>>
+
+>>> spam = list(range(0,100,3))#step thrice from 0 to 99 and assign the list to spam
+
 >>> spam
 [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99]
 >>> 
 >>> 
 >>> supplies = ['pens', 'staplers', 'binders', 'pins']
 >>> 
->>> for i in range(len(supplies))
-SyntaxError: invalid syntax
 >>> for i in range(len(supplies)):
 	print('Index ' + str(i) + ' in supplies is: ' +supplies[i])
 
-	
+#Output:	
 Index 0 in supplies is: pens
 Index 1 in supplies is: staplers
 Index 2 in supplies is: binders
@@ -57,7 +58,7 @@ Index 3 in supplies is: pins
 >>> for i in range(len(supplies)):
 	print('Index ' + str(i) + ' in supplies is: ' +supplies[i])
 
-	
+#Output:	
 Index 0 in supplies is: pens
 Index 1 in supplies is: pens
 Index 2 in supplies is: pens
