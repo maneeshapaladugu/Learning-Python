3972e2d4aef0eee42f0f807427feea81af676cf3
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2021-01-26 22:59:55 +0530

    Add files via upload

--- Basic Concepts/Dictionary_DataType.py
+++ Basic Concepts/Dictionary_DataType.py
@@ -1,36 +1,46 @@
 Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
+
+
 >>> #Like a list, a dictionary is a collection of many values
 >>> #But unlike list indexes, dictionaries indexes can use many differential types
 >>> #Dictionaries indexes are called keys
 >>> #Dictionaries index with its associated value is called key value pair
+>>> #A key value pair is called as item
 >>> 
 >>> myCat = {'size':'fat', 'color':'grey', 'disposition':'loud'}
 >>> myCat
 {'size': 'fat', 'color': 'grey', 'disposition': 'loud'}
+
 >>> myCat['size']
 'fat'
 >>> 
 >>> 
 >>> 'My cat has ' + myCat['color'] + ' tail'
 'My cat has grey tail'
+>>>
+
+#*******************************************************************************************************************
+
 >>> 
->>> 
->>> #Lists can have integer indexes only which starts with 0 by default
->>> #Dictionaries can have integer indexes but they don't have to start with 0
+>>> #Lists can have integer indexes only, which starts with 0 by default
+>>> #Dictionaries can have integer indexes but they do not have to start with 0
 >>> spam = {12345:'Luggage combination', 42:'The answer'}
 >>> 
 >>> #unlike lists, the items in dictionaries are unordered
->>> spam[0]
+>>> spam[0]#Error
 Traceback (most recent call last):
   File "<pyshell#18>", line 1, in <module>
     spam[0]
 KeyError: 0
+
 >>> spam[12345]
 'Luggage combination'
 >>> spam[42]
 'The answer'
->>> 
+>>>
+
+#*******************************************************************************************************************
 >>> 
 >>> [1,2,3] == [3,2,1] #Though contents are same, order is different here and thus these lists are not equal
 False
@@ -41,26 +51,19 @@ False
 True
 >>> #Unlike lists, while comparing dictionaries, order is not important
 >>> 
->>> 
->>> eggs['color']
-Traceback (most recent call last):
-  File "<pyshell#31>", line 1, in <module>
-    eggs['color']
-KeyError: 'color'
->>> 
->>> 
->>> 'color' in eggs #using in operator
+
+>>> 'color' in eggs #using 'in' operator to check if a key exists in a dictionary or not#Should provide keys, not values
 False
 >>> 'name' in eggs
 True
 >>> 'name' not in eggs
 False
 >>> 
->>> 
+>>> #*******************************************************************************************************************
+
 >>> #Dictionaries are mutable like lists
->>> #Variables hold references to dictionary values
->>> 
->>> #Dictionaries methods: keys(), values(), items():
+>>> #Variables hold references to dictionary values 
+>>> #Dictionary methods: keys(), values(), items():
 >>> list(eggs.keys())
 ['name', 'species', 'age']
 >>> list(eggs.values())
@@ -71,10 +74,11 @@ False
 >>> 
 >>> eggs.keys()
 dict_keys(['name', 'species', 'age'])
+
 >>> for k in eggs.keys():
 	print(k)
 
-	
+#Output:	
 name
 species
 age
@@ -83,26 +87,16 @@ age
 >>> for v in eggs.values():
 	print(v)
 
-	
+#Output:	
 Zophie
 cat
 8
 >>> 
->>> for k,v in eggs.items():
-	print(i)
 
-	
-Traceback (most recent call last):
-  File "<pyshell#60>", line 2, in <module>
-    print(i)
-NameError: name 'i' is not defined
->>> 
->>> 
->>> 
->>> for k,v in eggs.items():
-	print(k,v)
+>>> for m,n in eggs.items():
+	print(m,n)
 
-	
+#Output:	
 name Zophie
 species cat
 age 8
@@ -111,12 +105,15 @@ age 8
 >>> for i in eggs.items():
 	print(i)
 
-	
+#Output:	
 ('name', 'Zophie')
 ('species', 'cat')
 ('age', 8)
 >>> 
->>> 
+>>>
+
+#*******************************************************************************************************************
+
 >>> eggs
 {'name': 'Zophie', 'species': 'cat', 'age': 8}
 >>> 
@@ -137,24 +134,28 @@ KeyError: 'color'
 >>> if 'name' in eggs:
 	print(eggs['name'])
 
-	
+#Output:	
 Zophie
+>>>
+
+#*******************************************************************************************************************
+
 >>> 
->>> 
->>> #Alternate to above if: using get() method
+>>> #Alternate to 'in' operator in above case: using get() method
 >>> eggs
 {'name': 'Zophie', 'species': 'cat', 'age': 8}
->>> eggs.get('age', 0) #2nd arg species the default value to be returned if key doesn't exists in dictionary
+>>> eggs.get('age', 0) #2nd arg specifies the default value to be returned if key doesn't exists in dictionary
 8
 >>> 
 >>> eggs.get('color','')#Return blank string if color key doesn't exists
 ''
+
 >>> picnicItems = {'apples':5, 'cups':2}
 >>> print('I am bringing ' + str(picnicItems.get('napkins',0)) + ' napkins to the picnic')
 I am bringing 0 napkins to the picnic
 >>> 
 >>> 
->>> print('I am bringing ' + str(picnicItems['napkins']) + ' napkins to the picnic')
+>>> print('I am bringing ' + str(picnicItems['napkins']) + ' napkins to the picnic')#Error
 Traceback (most recent call last):
   File "<pyshell#97>", line 1, in <module>
     print('I am bringing ' + str(picnicItems['napkins']) + ' napkins to the picnic')
@@ -170,7 +171,10 @@ KeyError: 'napkins'
 >>> eggs
 {'name': 'Zophie', 'species': 'cat', 'age': 8, 'color': 'black'}
 >>> 
->>> 
+>>>
+
+#*******************************************************************************************************************
+
 >>> #Alternate to above if condition:
 >>> 
 >>> eggs = {'name': 'Zophie', 'species': 'cat', 'age': 8}
@@ -184,5 +188,5 @@ KeyError: 'napkins'
 'black'
 >>> eggs
 {'name': 'Zophie', 'species': 'cat', 'age': 8, 'color': 'black'}
->>> #setdefault sets key value pair only if a key doesn't exists
+>>> #setdefault sets key value pair only if a key doesn't exists in given dictionary 
 >>> 
