edc26cff8fa1dedd123ba78fa17eea58d0aed62d
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2021-01-26 16:37:14 +0530

    Add files via upload

--- Basic Concepts/Global and local scopes.py
+++ Basic Concepts/Global and local scopes.py
@@ -1,7 +1,7 @@
 spam = 42 #global variable
 
-def eggs():
-    spam = 42 #local variable
+def fun1():
+    spam = 40 #local variable
     print(spam)
 
 print('Some code here')#global
@@ -11,10 +11,10 @@ print('Some code here')#global
 
 #To assign a value to global variable in function scope:
 
-def eggs2():
-    global spam #if we do not inform spam is global, spam will be created in eggs2 local scope as a local variable
+def fun2():
+    global spam #if we do not inform spam is global, spam will be created in fun2 local scope as a local variable
     spam = 50
     print(spam)
 
-eggs()
-eggs2()
+fun1()
+fun2()
