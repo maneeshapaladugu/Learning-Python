69f503273108a6b195cadc3313295f893cfd970e
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2021-02-13 17:12:08 +0530

    Add files via upload

---
+++ Practice/Sum_Dict_Values.py
@@ -0,0 +1,5 @@
+#Python program to sum all the items (values) in dictionary
+
+dict =  {'a':10, 'b':20, 'c':30}
+print("Sum of values in the given dictionary is:")
+print(sum(dict.values())) #dictionary method values() returns a view of all values in dictionary 
