90bb8ad88518ea790111f5e8b32efc66ad0e45bb
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2021-02-17 22:45:16 +0530

    Add files via upload

--- Advanced Concepts/Reg_exp_To_Find_Phone_Numbers_.py
+++ Advanced Concepts/Reg_exp_To_Find_Phone_Numbers_.py
@@ -8,4 +8,14 @@ phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') #We usually pass raw strin
 mo = phoneNumRegex.search(message) # regex data type has a search() method Searching a message and it returns a match object 
 print(mo.group()) #Match object has a method group() to tell you the actual string
 
-#Output: 415-555-1011
+#Output: 415-555-1011  #found the first occurrence of the pattern
+
+
+########################################################################################################
+
+#findall() method
+
+phoneNumberRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+print(phoneNumRegex.findall('Call me at 415-555-1011 tomorrow, or at 415-555-9999'))
+
+#Output: ['415-555-1011', '415-555-9999']
