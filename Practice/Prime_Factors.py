05a5e1984cc3aaacafb38a54939a9d06c6ac0c29
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2021-02-17 20:13:18 +0530

    Add files via upload

--- Practice/Prime_Factors.py
+++ Practice/Prime_Factors.py
@@ -4,15 +4,15 @@ def isPrime(num):
     if num == 2:
         return 1
 
-    for i in range(2,num):
+    for i in range(2,num): #Here loop starts with i=2 and ends with i=num-1
         if num % i == 0:
             return 0
     return 1
 
 num = int(input("Enter a number:\n"))
-print("The prime factors of %d are:\n" %(num))
+print("The prime factors of %d are:" %(num))
 
-for i in range(2,num//2+1):
+for i in range(2,num//2+1): #as the loop ends with one number backward, we are adding one to it
     if num % i == 0:
         if isPrime(i):
             print(i)
